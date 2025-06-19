ae0bf0a56aa1ecee6d7e209971da91ce19eafeb9
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2025-06-18 21:24:32 -0300

    'AboutSection: terminado'

--- src/pages/homepage/AboutSection.tsx
+++ src/pages/homepage/AboutSection.tsx
@@ -1,12 +1,23 @@
 import type { JSX } from "react";
 
-import styles from './AboutSection.module.css';
+import styles from "./AboutSection.module.css";
 
 function AboutSection(): JSX.Element {
-
-    return (<div className={styles.about}>
-
-    </div>)
+    return (
+        <div className={styles.about}>
+            <h2>Sobre mim</h2>
+            <p>
+                Sou um desenvolvedor Full-Stack com experiência em diversos
+                projetos, buscando sempre o equilíbrio entre um design
+                chamativo, usabilidade e acessibilidade.
+            </p>
+            <p>
+                Trabalho usando tecnologias modernas como React, TypeScript e
+                Node.JS, criando interfaces agradáveis e eficientes que funcionam
+                em qualquer dispositivo. 
+            </p>
+        </div>
+    );
 }
 
 export default AboutSection;
