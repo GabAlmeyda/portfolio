6857b0628bf5e8286e9f7e3556e8ac08be9f1991
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2025-06-29 09:37:00 -0300

    Biblioteca 'emailjs-com' trocada por '@emailjs/browser'

--- src/page/Contact.tsx
+++ src/page/Contact.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, type ChangeEvent, type FormEvent, type JSX } from "react";
-import emailjs from "emailjs-com";
+import emailjs from "@emailjs/browser";
 
 import styles from "./Contact.module.css";
 
