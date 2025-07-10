0f0559d5fe40f939d28c2bbd0f908a2a60f958cd
Gabriel Almeida <gabrielalmeida7258@gmail.com>
2025-07-10 18:20:18 -0300

    Remoção da seção 'Projects'

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Page from "./components/layout/Page";
 import Hero from "./page/Hero";
 import About from "./page/About";
-import Projects from "./page/Projects";
 import Contact from "./page/Contact";
 import Tools from "./page/Tools";
 
@@ -13,7 +12,6 @@ function App() {
             <Hero />
             <About />
             <Tools />
-            <Projects />
             <Contact />
         </Page>
     );
